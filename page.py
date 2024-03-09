31c194614eaa0e216276c8aff9afd7cdbf59b156
alexatallo <alexatallo@oakland.edu>
2024-03-09 18:30:33 -0500

    random book try

--- page.py
+++ page.py
@@ -4,10 +4,11 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import string
-nltk.download('punkt')
-nltk.download('wordnet')
 import requests
 from googleapiclient.discovery import build
+
+nltk.download('punkt')
+nltk.download('wordnet')
 
 app = Flask(__name__)
 
@@ -22,7 +23,7 @@ GOOGLE_BOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes'
 # Route for serving the HTML file
 @app.route('/')
 def home():
-    return render_template('interface.html')
+    return render_template('pageInterface.html')
 
 #this is a function to preprocess text using NLTK
 def preprocess_text(text):
@@ -53,7 +54,7 @@ def chat():
     user_input = request.form['input']
     # Process user input and generate response
     response = "This is a response to your input: " + user_input
-    return render_template('interface.html', response=response)
+    return render_template('pageInterface.html', response=response)
 
 
 # Route for random book recommendations
@@ -61,6 +62,7 @@ def chat():
 def random_book():
     # Parameters for the API request
     params = {
+        'q': 'random',
         'key': google_books_api_key,
         'orderBy': 'relevance',
         'maxResults': 1  # Get only one random book
@@ -91,10 +93,28 @@ def random_book():
 # Route for displaying book details
 @app.route('/book-details/<book_id>')
 def book_details(book_id):
-    # Add logic to fetch and display book details from the Google Books API
-    # Render the book details template with book information
+    # Make a GET request to the Google Books API to fetch book details
+    book_url = f"{GOOGLE_BOOKS_API_URL}/{book_id}"
+    response = requests.get(book_url, params={'key': google_books_api_key})
+
+    if response.status_code == 200:
+        # Extract book information from the API response
+        book_info = response.json()
+
+        # Extract relevant book details
+        book_title = book_info['volumeInfo']['title']
+        book_author = ', '.join(book_info['volumeInfo'].get('authors', ['Unknown Author']))
+        book_description = book_info['volumeInfo'].get('description', 'No description available')
+        book_cover = book_info['volumeInfo']['imageLinks']['thumbnail'] if 'imageLinks' in book_info['volumeInfo'] else ''
+
+        # Render the book details template with book information
+        return render_template('book_details.html', title=book_title, author=book_author,
+                               description=book_description, cover=book_cover)
+    else:
+        # Return an error message if the request to the Google Books API fails
+        return jsonify({'error': 'Failed to fetch book details'}), response.status_code
 
 
 
-     if __name__ == '__main__':
-            app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
